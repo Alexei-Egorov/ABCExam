2061bcb6f7b8c33acf8f4d829686fe02447bb892
Alexei-Egorov <fraine21@gmail.com>
2026-02-04 17:55:48 +0100

    Init project, add .gitignore

---
+++ ABCExam/ABCExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABCExamApp.swift
+//  ABCExam
+//
+//  Created by Aliaksei Yahorau on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ABCExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
